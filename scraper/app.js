2887adb0d3afd14cdb897c872e69cf4549d079c8
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2017-03-17 23:17:56 -0400

    Retrieve and print raw email objects from Gmail API
    - Gets all unread emails and prints the object to console
    - TODO: Format the object

---
+++ scraper/app.js
@@ -0,0 +1,143 @@
+/******************************************************************************/
+/* app.js                                                                     */
+/* Author: Seung Jae Lee                                                      */
+/* Parts of code from                                                         */
+/* https://developers.google.com/google-apps/calendar/quickstart/node         */
+/******************************************************************************/
+
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+// read/write access except delete for gmail, and read/write access to calendar
+var SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+        process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-foodmap.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // main program.
+    authorize(JSON.parse(content), main);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var auth = new googleAuth();
+    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function(code) {
+        rl.close();
+        oauth2Client.getToken(code, function(err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
+        }
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Parses events from all unread emails and prints them stdout
+ *
+ * @param {Object} auth Authorization credentials for Google APIs.
+ */
+var main = function (auth) {
+    // set auth as a global default
+    google.options({auth: auth});
+    
+    google.gmail('v1').users.messages.list({ // Get unread message list
+        userId: 'me',
+        q: 'is:unread',
+    }, function (err, res) {
+
+        // if unread message exists
+        if (!err && res && res.messages && res.messages.length) {
+            for(var i = 0; i < res.messages.length; i++) {
+                var messageId = res.messages[i].id;
+
+                // Temporarily disabled for testing
+                // Mark email as read by deleting UNREAD label
+                // google.gmail('v1').users.messages.modify({
+                //     userId: 'me',
+                //     id: messageId,
+                //     resource: { removeLabelIds: ['UNREAD'] },
+                // });
+
+                // Get content of email
+                google.gmail('v1').users.messages.get({
+                    userId: 'me',
+                    id: messageId,
+                }, function(err, result) {
+                    console.log(result.payload);
+                });
+            }
+        } else {
+            console.log('No unread message exists');
+        }
+    });
+};
