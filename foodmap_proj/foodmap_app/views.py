dc44592a60c2dded6cd1140145d79a159cbd6ad0
Michael J Friedman <mfred240@gmail.com>
2017-03-19 23:49:26 -0400

    Added a view for the landing page - simple "hello world"
    Includes the view handler in foodmap_app/views.py and a test in
    foodmap_app/tests.py to make sure it works. Passes the test.

--- foodmap_proj/foodmap_app/views.py
+++ foodmap_proj/foodmap_app/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    '''
+    Simple view for the landing page; returns a "hello world" page.
+    '''
+    return HttpResponse('Hello, world!')
