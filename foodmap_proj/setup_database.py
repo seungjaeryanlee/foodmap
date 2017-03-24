e26a3e633d09a43042868c5fb0a4c248f9df3205
Michael J Friedman <mfred240@gmail.com>
2017-03-23 22:21:18 -0400

    Updated setup script and README again for easier use
    The previous populate_locations_table.py script is instead part
    of setup_database.py. You can use this instead to to populate the
    Locations table. (There are still small changes that need to be
    made before this actually populates the table, as indicated in
    the script.)

--- foodmap_proj/setup_database.py
+++ foodmap_proj/setup_database.py
@@ -1,25 +1,31 @@
 #-------------------------------------------------------------------------------
 # setup_database.py
 # Author: Michael Friedman
+# Usage: python setup_database.py [-p sample|real]
 #
 # Runs the necessary Django commands to intialize/update the project's database.
 # You can also rerun it each time the models for the database are updated as a
 # shortcut for makemigrations + migrate. Optionally takes the -p argument to
-# auto-populate with some dummy data.
+# auto-populate with either sample data or real data.
 #
-# NOTE: Assumes this script is located in the setup_scripts directory. All paths
-# used in the script depend on that.
+# NOTE: Populating with real data requires scraping GPS coordinates for every
+# location at Princeton, so it will take a substantial amount of time.
 #-------------------------------------------------------------------------------
 
 # Check usage
 import sys
-usage = 'usage: python setup_database.py [-p]'
-if len(sys.argv) != 1 and len(sys.argv) != 2:
+usage = 'usage: python setup_database.py [-p sample|real]'
+if len(sys.argv) != 1 and len(sys.argv) != 3:
     print usage
     exit(1)
-elif len(sys.argv) == 2 and sys.argv[1] != '-p':
+elif len(sys.argv) == 3 and sys.argv[1] != '-p':
     print usage
     exit(1)
+elif len(sys.argv) == 3 and not sys.argv[2] in ['sample', 'real']:
+    print usage
+    exit(1)
+
+populate = None if len(sys.argv) != 3 else sys.argv[2]
 
 #-------------------------------------------------------------------------------
 
@@ -31,16 +37,46 @@ django.setup()
 
 #-------------------------------------------------------------------------------
 
-# Ok now we can get started...
-from django.utils import timezone
-from foodmap_app.models import Location, Offering
+def populate_locations_table(): # TODO: Update this method according to TODOs after testing
+    from selenium import webdriver
+    driver = webdriver.PhantomJS()
 
-# Initialize database
-os.system('python manage.py makemigrations foodmap_app')
-os.system('python manage.py migrate')
+    # Read in all location names from locations.txt
+    locations_file = open('locations.txt', 'r')
+    locations = [location.strip() for location in locations_file.readlines()]
 
-# Populate with some dummy data, if -p arg was given
-if len(sys.argv) > 1:
+    # Get lat/lng coordinates for each location from m.princeton.edu's map
+    # for i in range (0, 897 + 1):
+    for i in range(0, 20): # TODO: replace this line with the one above it after testing
+        driver.get('http://m.princeton.edu/map/detail?feed=91eda3cbe8&group=princeton&featureindex=' + str(i) + '&category=91eda3cbe8%3AALL&_b=%5B%7B%22t%22%3A%22Map%22%2C%22lt%22%3A%22Map%22%2C%22p%22%3A%22index%22%2C%22a%22%3A%22%22%7D%5D#')
+
+        # Skip this page if the location is not one that we want
+        location = driver.find_element_by_css_selector('h2.nonfocal').text.lower()
+        # if not location in locations: # TODO: uncomment this after testing
+        #     continue
+
+        # Go to "Directions" tab
+        directions_tab = driver.find_element_by_link_text('Directions')
+        directions_tab.click()
+
+        # Extract lat/lng coordinates from "View in Google Maps" button
+        # Link is of the form:
+        # http://maps.google.com? ... &q=loc:LAT,LNG+ ...
+        button = driver.find_element_by_link_text('View in Google Maps')
+        link = button.get_attribute('href')
+        lat_str = link[link.index('q=loc:')+len('q=loc:'):link.index(',')]
+        lng_str = link[link.index(',')+1:link.index('+')]
+        lat = float(lat_str[:6])
+        lng = float(lng_str[:7])
+
+        # TODO: Insert entry into table
+        print '%s\t|\t%s\t|\t%s' % (location, str(lat), str(lng))
+
+    driver.quit()
+
+#-------------------------------------------------------------------------------
+
+def populate_sample_data():
     princeton = Location(name='Princeton University', lat=40.343, lng=-74.653)
     harvard = Location(name='Harvard University', lat=42.377, lng=-71.118)
     yale = Location(name='Yale University', lat=41.316, lng=-72.924)
@@ -57,3 +93,25 @@ if len(sys.argv) > 1:
     ]
     for o in offerings:
         o.save()
+
+#-------------------------------------------------------------------------------
+
+### Main script
+
+from django.utils import timezone
+from foodmap_app.models import Location, Offering
+
+# Initialize database
+print 'Initializing/updating database...'
+os.system('python manage.py makemigrations foodmap_app')
+os.system('python manage.py migrate')
+print 'Done!'
+
+# Handle -p arg
+if populate:
+    print '\nPopulating database...'
+    if populate == 'sample':
+        populate_sample_data()
+    else:
+        populate_locations_table()
+    print 'Done!'
