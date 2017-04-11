7f6bff0bf44dd73af28ea5a5e72cfd5227127259
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2017-04-11 15:54:04 -0400

    Configure to use whitenoise

--- foodmap_proj/settings.py
+++ foodmap_proj/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Base directory where files/images in the database are actually stored.
--- foodmap_proj/wsgi.py
+++ foodmap_proj/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.djnago import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodmap_proj.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
