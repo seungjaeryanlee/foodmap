251447f5c7e7498eb84d02a2fc7cae7362954c0f
Nathan Mytelka <Forgind@users.noreply.github.com>
2017-04-18 20:55:25 -0400

    Changed how hours/minutes were formatted
    1hour, 0 minutes was changed to 60 minutes (and it now flips over at 61 minutes).

--- foodmap_app/static/js/mapbuilder.js
+++ foodmap_app/static/js/mapbuilder.js
@@ -64,7 +64,7 @@
                             ]
                         },
                         "properties": {
-                            "popupContent": "<b>" + response_offerings[i].location.name + "</b><br><i>"+ response_offerings[i].title + "</i><br>" + (parseFloat(response_offerings[i].minutes) >= 60? "1 hour, "+(parseFloat(response_offerings[i].minutes)-60): response_offerings[i].minutes) + " minutes old",  // by default this is just location's name
+                            "popupContent": "<b>" + response_offerings[i].location.name + "</b><br><i>"+ response_offerings[i].title + "</i><br>" + (parseFloat(response_offerings[i].minutes) > 60? "1 hour, "+(parseFloat(response_offerings[i].minutes)-60): response_offerings[i].minutes) + " minutes old",  // by default this is just location's name
                             "extra": response_offerings[i].description
                         },
                         "id": i
