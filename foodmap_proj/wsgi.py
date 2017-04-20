a812a72a598cca4e21f87f2feaa38ada550c787f
Michael J Friedman <mfred240@gmail.com>
2017-04-19 23:01:00 -0400

    Adjusted wsgi.py to use separate dev/prod settings

--- foodmap_proj/wsgi.py
+++ foodmap_proj/wsgi.py
@@ -9,10 +9,17 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+from django.core.exceptions import ImproperlyConfigured
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodmap_proj.settings")
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+# Configure differently based on whether we're in development or production mode
+if os.environ['DJANGO_SETTINGS_MODULE'] == 'foodmap_proj.settings.development':
+    pass
+elif os.environ['DJANGO_SETTINGS_MODULE'] == 'foodmap_proj.settings.production':
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(application)
+else:
+    raise ImproperlyConfigured('DJANGO_SETTINGS_MODULE not set. Did you activate the virtual environment?')
