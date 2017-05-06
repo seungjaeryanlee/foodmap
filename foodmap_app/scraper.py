56a9b7d687e809d75cb2de661ad718bed0bbad45
Michael J Friedman <mfred240@gmail.com>
2017-05-06 12:26:01 -0400

    Implemented Python side of scraper interface
    - For this to work, it also requires a Node end of the interface.
      This has not yet been implemented (only Python side).

--- foodmap_app/scraper.py
+++ foodmap_app/scraper.py
@@ -6,9 +6,19 @@ This is a Python interface into the NodeJS scraper, for the purpose of using
 Python to request that something be scraped.
 '''
 
+from subprocess import Popen, PIPE
+
 def get_food(text):
     '''
     Get all foods that are in 'text'. Return them in a string as a
     comma-separated list, with the first letter of the first food capitalized.
     '''
-    return 'Pizza'  # TODO: Implement get_food()
+    # Start new process for NodeJS scraper. Pipe stdin/stdout between this
+    # process and the NodeJS process to send data between them.
+    node_program_name = ['node', 'PROGRAM_NAME'] # TODO: insert program name
+    node_process = Popen(node_program_name, shell=True, stdin=PIPE, stdout=PIPE)
+    node_process.stdin.write(text)
+    node_process.stdin.close()
+    node_process.wait()
+    return node_process.stdout.read()
+
